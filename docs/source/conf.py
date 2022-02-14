9e3af73899bd17dce28e42ed5de606025924941e
Marco <marco@parretto.com>
2022-02-14 11:01:12 +0100

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'DisplayPort'
+copyright = '2022 by Parretto B.V.'
+author = 'Parretto'
 
 release = '0.1'
 version = '0.1.0'
